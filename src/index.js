1b554126c4ccc17efd3ec8bc6d944bb34938a354
ahmedsharur <ahmedshafeysharur@gmail.com>
2021-09-23 13:24:49 -0500

    added redux-reac

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
+
+
+const bookList = (state = [], action) => {
+    // TODO - set book list with data from server
+    if (action.type === 'SET_BOOK_LIST') {
+      return action.payload;
+    }
+    // No change to data
+    return state;
+  }
+
 ReactDOM.render(<App />, document.getElementById('root'));
