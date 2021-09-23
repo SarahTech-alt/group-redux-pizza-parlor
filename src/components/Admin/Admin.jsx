9251bb40753b641dd8d4d39e34808721fc973a01
Sarah Fuoss <sfuoss@gmail.com>
2021-09-23 14:03:44 -0500

    Added table to Admin page.

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,10 +1,36 @@
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 function Admin(){
-    //Get request for orders 
     
+    const orderInfo() => {
+        
+    }
+
+    const customerInfo = useSelector (store => store.orderReducer)
     return(
         <>
+        <h1>Step 3: Checkout</h1>
+        <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Time Order Placed</th>
+            <th>Type</th>
+            <th>Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reduxStore.checkoutReducer.map((item, i) =>
+            (<tr key={i}>
+              <td>{orderReducer.name}</td>
+              <td>{orderReducer.time}</td>
+              <td>{orderReducer.type}</td>
+              <td>{orderReducer.cost}</td>
+            </tr>)
+          )}
+        </tbody>
+      </table>
         </>
     )
 }
