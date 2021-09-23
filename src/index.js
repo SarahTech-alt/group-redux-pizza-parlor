e54525a4f479006caf820356eda8df66be24ef65
ahmedsharur <ahmedshafeysharur@gmail.com>
2021-09-23 13:45:35 -0500

    updated ge t app and index

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'; 
+import logger from 'redux-logger';
 
 
 
-const bookList = (state = [], action) => {
+
+const pizzaList = (state = [], action) => {
     // TODO - set book list with data from server
-    if (action.type === 'SET_BOOK_LIST') {
+    if (action.type === 'SET_PIZZA_LIST') {
       return action.payload;
     }
     // No change to data
